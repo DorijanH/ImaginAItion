29865efdb13421c2b2c513da2d0cf9dee036eaf0
DorijanH <dhaspl@gmail.com>
2024-08-05 23:20:27 +0200

    feat: Setup shadcn-ui

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button size="sm">
+        Click Me Twice
+      </Button>
+    </div>
+  );
+}
